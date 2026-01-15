24620fa83ca9a1161b20574279d4c901811563f4
realjokele <misc@jokele.de>
2026-01-15 16:23:09 +0100

    No vim global error anymore

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,7 +1,20 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    { "mason-org/mason.nvim", opts = {}},
-  }
+    { "mason-org/mason.nvim", opts = {} },
+  },
+  config = function()
+    vim.lsp.config("lua_ls", {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    })
 
+    vim.lsp.enable("lua_ls")
+  end,
 }
+

4081f7eb35485f08a746cff95bb169e01a93938c
realjokele <misc@jokele.de>
2026-01-15 22:14:46 +0100

    Setup which_key

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,19 +1,8 @@
 return {
 	"stevearc/oil.nvim",
-	---@module 'oil'
-	---@type oil.SetupOpts
 	opts = {},
 	-- Optional dependencies
 	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-	keys = {
-		{
-			"ö",
-			function()
-				require("oil").open_float()
-			end,
-			desc = "Open oil",
-		},
-	},
 	config = function()
 		require("oil").setup({
 			float = {
@@ -21,5 +10,7 @@ return {
 				padding = 5,
 			},
 		})
+
+		vim.keymap.set("n", "<space>ö", require("oil").toggle_float, { desc = "Open oil" })
 	end,
 }

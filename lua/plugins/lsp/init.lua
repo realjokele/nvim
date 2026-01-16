c81580ba9b2af9b8006378f0b6a454f67a642670
realjokele <misc@jokele.de>
2026-01-16 15:13:40 +0100

    Working treesitter

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -36,6 +36,19 @@ return {
 						},
 					},
 				})
+
+				vim.lsp.enable({ "lua_ls", "ts_ls", "gopls", "eslint" })
+				vim.api.nvim_create_autocmd("LspAttach", {
+					group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+					callback = function(ev)
+						local opts = { buffer = ev.buf }
+						vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+						vim.keymap.set("n", "<leader><space>", vim.lsp.buf.hover, opts)
+						vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+						vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+						vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					end,
+				})
 			end,
 		},
 	},
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,39 +1,41 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	lazy = false,
-	config = function()
-		require("nvim-treesitter").setup({
-			ensure_installed = {
-				"bash",
-				"c",
-				"css",
-				"html",
-				"javascript",
-				"json",
-				"latex",
-				"lua",
-				"luadoc",
-				"luap",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"regex",
-				"tsx",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"yaml",
-			},
-			auto_install = true,
-		})
-		-- Enable treesitter-based highlighting and indentation
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function()
-				pcall(vim.treesitter.start)
-			end,
-		})
-		vim.opt.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false,
+    config = function()
+      require("nvim-treesitter").setup({
+        ensure_installed = {
+          "bash",
+          "c",
+          "css",
+          "html",
+          "javascript",
+          "json",
+          "lua",
+          "luadoc",
+          "luap",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "query",
+          "regex",
+          "scss",
+          "tsx",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "yaml",
+        },
+        auto_install = true,
+      })
+      -- Enable treesitter-based highlighting and indentation
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
+      vim.opt.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+
 }
+

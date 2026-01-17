b3d7ef4847b610aabc5b790251df06575d31ac54
realjokele <misc@jokele.de>
2026-01-17 10:56:41 +0100

    Add comments

--- lua/config/ftplugin.lua
+++ lua/config/ftplugin.lua
@@ -1,18 +1,23 @@
-local M = {}
+local Module = {}
 
-function M.indent(size)
-  vim.opt_local.tabstop = size
-  vim.opt_local.shiftwidth = size
-  vim.opt_local.softtabstop = size
-  return M
+function Module.indent(size)
+	vim.opt_local.tabstop = size
+	vim.opt_local.shiftwidth = size
+	vim.opt_local.softtabstop = size
+	return Module
 end
 
-function M.treesitter()
-  vim.treesitter.start()
-  vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-  vim.wo[0][0].foldmethod = 'expr'
-  vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-  return M
+function Module.treesitter()
+	-- treesitter highlighting
+	vim.treesitter.start()
+
+	-- treesitter indention
+	vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+	-- treesitter folding
+	vim.wo[0][0].foldmethod = "expr"
+	vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+	return Module
 end
 
-return M
+return Module

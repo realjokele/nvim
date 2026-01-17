8b1e9a8446ea206eeb5cb90687c4d147642485e2
realjokele <misc@jokele.de>
2026-01-17 15:47:29 +0100

    Add some mini tools

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		{ "nvim-mini/mini.pairs", version = "*", opts = {} },
+		{ "nvim-mini/mini.cursorword", version = "*", opts = {} },
+		{
+			"nvim-mini/mini.indentscope",
+			version = "*",
+			opts = {},
+		},
+	},
+}

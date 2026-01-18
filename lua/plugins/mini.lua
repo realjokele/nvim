fb56986315f566ae36984ac637afba11723a0f28
realjokele <misc@jokele.de>
2026-01-18 14:24:20 +0100

    Add mini git/diff

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,5 +7,27 @@ return {
 			version = "*",
 			opts = {},
 		},
+		{
+			"nvim-mini/mini-git",
+			version = "*",
+			opts = {},
+			config = function()
+				require("mini.git").setup()
+			end,
+		},
+		{
+			"nvim-mini/mini.diff",
+			version = "*",
+			config = function()
+				vim.api.nvim_set_hl(0, "MiniDiffSignAdd", { fg = "#A6E22E", bold = true }) -- Lime Green
+				vim.api.nvim_set_hl(0, "MiniDiffSignChange", { fg = "#66D9EF", bold = true }) -- Sky Blue
+				vim.api.nvim_set_hl(0, "MiniDiffSignDelete", { fg = "#F92672", bold = true }) -- Electric Pink/Red
+				require("mini.diff").setup({
+					view = {
+						style = "sign",
+					},
+				})
+			end,
+		},
 	},
 }
--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -15,6 +15,7 @@ return {
 					local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
 					local lsp = statusline.section_lsp({ trunc_width = 75 })
 					local git = statusline.section_git({ trunc_width = 75 })
+					local diff = statusline.section_diff({ trunc_width = 75 })
 					local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
 					local filename = statusline.section_filename({ trunc_width = 140 })
 					local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
@@ -23,7 +24,7 @@ return {
 					-- Return the combined string using MiniStatusline.combine_groups
 					return statusline.combine_groups({
 						{ hl = mode_hl, strings = { mode } },
-						{ hl = "MiniStatuslineDevinfo", strings = { git, diagnostics, lsp } },
+						{ hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
 
 						"%<", -- Mark for truncating
 						{ hl = "MiniStatuslineFilename", strings = { filename } },

3d4bc9379c76ba400fff84e6d92d30b8d9d8fca5
realjokele <misc@jokele.de>
2026-01-18 05:43:49 +0100

    Add format on save

--- lua/plugins/formating.lua
+++ lua/plugins/formating.lua
@@ -16,6 +16,10 @@ return {
 					-- You can customize some of the format options for the filetype (:help conform.format)
 					-- Conform will run the first available formatter
 					javascript = { "prettierd", "prettier", stop_after_first = true },
+					javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+					typescript = { "prettierd", "prettier", stop_after_first = true },
+					typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+					json = { "prettierd", "prettier", stop_after_first = true },
 				},
 			})
 		end,

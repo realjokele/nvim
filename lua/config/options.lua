cbedd519283f3139073323ffb93a3d71b7517d31
realjokele <misc@jokele.de>
2026-01-18 15:24:34 +0100

    Add autocmds

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -62,16 +62,6 @@ vim.opt.splitright = true
 -- Faster scrolling
 --vim.opt.lazyredraw = true
 
--- Highlight yank
-vim.api.nvim_create_autocmd("textyankpost", {
-	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
-	pattern = "*",
-	desc = "highlight selection on yank",
-	callback = function()
-		vim.highlight.on_yank({ timeout = 200, visual = true })
-	end,
-})
-
 -----------------------------------------------------------
 -- Search Config
 -----------------------------------------------------------
